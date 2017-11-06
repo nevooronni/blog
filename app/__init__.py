e5ad359be91c31d0ae0ad9714798ae470a5fc09e
Neville Oronni <nevooronni@gmail.com>
2017-11-06 22:26:40 +0300

    addedclass model for our blog post

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,20 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail#extension provides a simpole interface to setup smtop with your app send messages from your views and scripts
+from flask_simplemde import simpleMDE#extension that helps flask intergrate simpleMDE markdown editor to your flask appliation 
+
+#initializing flask extension
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+	app = Flask(__name__)
+
+	#app configurations
+	app.config.from_object(config_options[config_name])
+	db.init_app(app)
+
+	#initializing bootstrap
+	bootstrap.init_app(app)
---
+++ app/models.py
@@ -0,0 +1,36 @@
+from . import db
+
+class BlogPost(db.Model):
+	'''
+	class that creates object for a blog post
+	'''
+	__tablename__ = 'posts'
+
+	id = db.Column(db.Integer,primary_key=True)
+	title = db.Column(db.String)
+	post = db.Column(db.String)
+	date_posted = db.Column(db.DateTime,default=datetme.utcnow)
+
+	def save_BlogPost(self):
+	'''
+	function that saves the blog post to the db
+	'''
+	db.session.add(self)
+	db.session.commit()
+
+	@classmethod
+	def get_BlogPost(cls):
+		'''
+		function that gets a list of all the blog posts on the db
+		'''
+		list_of_blogs = BlogPost.query.all()
+		return list_of_blogs		
+
+	@classmethod
+	def delete_BlogPost(cls,BlogPost_id):
+	'''
+	function that deletes  a blog post from the db
+	'''
+		delete_blog = BlogPost.query.filter_by(id=BlogPost_id).delete()
+		db.session.commit()
+
